aa110d2076765b10e3a8d207b2fbe279ade84b5d
lian2377 <lian2377@gmail.com>
2015-11-24 21:38:24 +0800

    Add Setting Activity
    new activity
    change current activity
    dynamic add buttons

--- app/src/main/java/tw/edu/ntust/jojllman/wearableapplication/ChooseClassActivity.java
+++ app/src/main/java/tw/edu/ntust/jojllman/wearableapplication/ChooseClassActivity.java
@@ -1,7 +1,9 @@
 package tw.edu.ntust.jojllman.wearableapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseClassActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ChooseClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_class);
+    }
+
+    public void OnHelpClick(View view){
+        Intent intent = new Intent();
+        intent.setClass(this  , SettingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/tw/edu/ntust/jojllman/wearableapplication/SettingActivity.java
@@ -0,0 +1,41 @@
+package tw.edu.ntust.jojllman.wearableapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        // 取得 LinearLayout 物件
+        LinearLayout ll = (LinearLayout)findViewById(R.id.view_setting);
+
+        // 將 TextView 加入到 LinearLayout 中
+        TextView tv = new TextView(this);
+        tv.setText(R.string.str_device_list);
+        tv.setTextSize(50);
+        ll.addView( tv );
+
+        // 將 Button  加入到 LinearLayout 中
+        int count=10;
+        Button btn[] = new Button[count];
+        while(count>0){
+            btn[--count] = new Button(this);
+            btn[count].setText("裝置" + count);
+            btn[count].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SettingActivity.this.finish();
+                }
+            });
+            ll.addView( btn[count] );
+        }
+    }
+}
