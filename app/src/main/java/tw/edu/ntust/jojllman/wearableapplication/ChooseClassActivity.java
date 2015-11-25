6eeaae8c07c9308aebf973b5debfb20dddfc834d
lian2377 <lian2377@gmail.com>
2015-11-25 13:48:00 +0800

    修改按下使用協助的動作

--- app/src/main/java/tw/edu/ntust/jojllman/wearableapplication/ChooseClassActivity.java
+++ app/src/main/java/tw/edu/ntust/jojllman/wearableapplication/ChooseClassActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class ChooseClassActivity extends AppCompatActivity {
-
+    int click_count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,8 +15,12 @@ public class ChooseClassActivity extends AppCompatActivity {
     }
 
     public void OnHelpClick(View view){
-        Intent intent = new Intent();
-        intent.setClass(this  , SettingActivity.class);
-        startActivity(intent);
+        Toast.makeText(this,"說明訊息出現",Toast.LENGTH_SHORT).show();
+        if(click_count++>2){
+            Intent intent = new Intent();
+            intent.setClass(this  , SettingActivity.class);
+            startActivity(intent);
+            click_count=0;
+        }
     }
 }
